41efb79e4e59715677dc51f1e83fc86d6b99c76f
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-11-27 12:18:16 +0530

    commit done

--- expense-tracker/src/context/ExpenseContext.jsx
+++ expense-tracker/src/context/ExpenseContext.jsx
@@ -6,4 +6,8 @@ const initialState = {
   expenses: [],
   loading : false,
   error: null,
+};
+
+export const ExpenseProvider = ({children}) => {
+  return <ExpenseContext.Provider value={{}}>{children}</ExpenseContext.Provider>
 }
